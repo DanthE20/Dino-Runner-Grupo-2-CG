78cd1977d50c3a2a4ebc481fc1c3e0920f3fa520
Daniel Gonzalez <crispip20@gmail.com>
2022-12-15 11:35:22 -0500

    Class 1: Dinosaur
    Se implemento la acción DUCK con exito

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,71 @@
+import pygame
+from pygame.sprite import Sprite
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
+
+RUNNING_ACTION = "running"
+JUMPING_ACTION = "jumping"
+DUCKING_ACTION = "ducking"
+
+class Dinosaur(Sprite):
+    X_POS = 80
+    Y_POS = 310
+    Y_POS_DUCK = 340
+    JUMP_VELOCITY = 8.5
+
+    def __init__(self):
+        self. image = RUNNING[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
+        self.jump_velocity = self.JUMP_VELOCITY
+        self.step = 0
+        self.step_duck = 0
+
+        self.action = RUNNING_ACTION
+
+    def update(self, user_input):
+        if self.action == DUCKING_ACTION:
+            self.duck()
+        elif self.action == JUMPING_ACTION:
+            self.jump()
+        else: 
+            self.action == RUNNING_ACTION
+            self.run()
+
+        if self.action != JUMPING_ACTION:
+            if user_input[pygame.K_UP]:
+                self.action = JUMPING_ACTION
+            elif user_input[pygame.K_DOWN]:
+                self.action = DUCKING_ACTION
+            else:
+                self.action = RUNNING_ACTION
+
+        if self.step >= 9:
+            self.step = 0
+
+        if self.step_duck >= 9:
+            self.step_duck = 0
+
+    def run(self):
+        self.image = RUNNING[0] if self.step < 5  else RUNNING[1]
+        self.rect.y = self.Y_POS
+        self.step += 1
+        
+    def jump(self):
+        self.image = JUMPING
+        self.rect.y -= self.jump_velocity * 4
+        self.jump_velocity -= 0.8
+
+        if self.jump_velocity < -self.JUMP_VELOCITY:
+            self.rect.y = self.Y_POS
+            self.jump_velocity = self.JUMP_VELOCITY
+            self.action = RUNNING_ACTION
+    
+    def duck(self):
+        self.image = DUCKING[0] if self.step_duck < 5  else DUCKING[1]
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS_DUCK
+        self.step_duck += 1
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
